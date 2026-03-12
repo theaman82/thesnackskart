a780bbd27ade735b3b86b57b76d2e72c6cd23922
Sudhanshu-code-cyber <sudhanshuaryan2005@gmail.com>
2026-03-12 21:10:43 +0530

    Implement navbar component and update app layout to include it

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,29 @@
-<div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+<div class="bg-amber-50 flex w-full px-10 py-4 shadow-md">
+   <div class="flex w-full justify-between items-center">
+    <div>
+         <a href="#" class="text-amber-700 font-bold text-3xl tracking-tight hover:text-amber-600 transition-colors duration-300">
+            TheSnacksKart<span class="text-amber-500 text-2xl">.</span>
+        </a>
+    </div>
+    <div class="flex gap-8">
+        <a href="" class="font-medium text-gray-700 hover:text-amber-700 relative group">
+            About Us
+            <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-700 transition-all duration-300 group-hover:w-full"></span>
+        </a>
+        <a href="" class="font-medium text-gray-700 hover:text-amber-700 relative group">
+            Our Stores
+            <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-700 transition-all duration-300 group-hover:w-full"></span>
+        </a>
+        <a href="" class="font-medium text-gray-700 hover:text-amber-700 relative group">
+            Contact Us
+            <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-700 transition-all duration-300 group-hover:w-full"></span>
+        </a>
+    </div>
+    <button class="relative overflow-hidden group">
+        <a href="" class="inline-block px-6 py-2.5 bg-amber-600 text-white font-semibold rounded-lg  relative z-10">
+            Inquiry For Import
+        </a>
+        {{-- <span class="absolute bottom-0 left-0 w-full h-0.5 bg-black transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span> --}}
+    </button>
+   </div>
 </div>
